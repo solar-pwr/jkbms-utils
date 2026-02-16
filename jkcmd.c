9193904b044e0e7d63c0b55087dc6196a095cc28
solar-pwr <strnad.cv@gmail.com>
2026-02-16 08:54:05 +0100

    Add files via upload

--- jkcmd.c
+++ jkcmd.c
@@ -1,6 +1,20 @@
-// JK BMS Command-line utility v0.9
+//    JK BMS Command-line utility v0.9
+//    Copyright (C) 2025-2026 Jakub Strnad
 //
-// Jakub Strnad 2025
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// ===========================================================
 //
 // Endianness conversion not implemented.
 // Will only work on a little-endian machine (x86, x64, arm).
